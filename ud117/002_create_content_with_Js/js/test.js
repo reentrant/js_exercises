0b866fa3f4178365a18c494e7cccc8764f57fce9
Julian Ruiz <julian.ruiz@cirrus.com>
2019-07-08 09:22:56 -0500

    appendChild

--- ud117/002_create_content_with_Js/js/test.js
+++ ud117/002_create_content_with_Js/js/test.js
@@ -1,5 +1,8 @@
 /**
 What happens after running this code?
+Explanation:
+If an element already exists in the DOM and this element is passed to .appendChild(),
+the parent.appendChild() method will move it rather than duplicating it
 */
 
 var mainHeading = document.querySelector("#main");
@@ -7,7 +10,6 @@ var otherHeading = document.querySelector('#other');
 var excitedText = document.createElement('span');
 
 excitedText.textContent = '!!!';
-
 mainHeading.appendChild(excitedText);
 otherHeading.appendChild(excitedText); //only otherHeading has three exclamation marks!
 
